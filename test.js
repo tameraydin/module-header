96af915abe240e4c617557a7589cc6bb3d7ff6bc
Tamer Aydin <tamer.aydin@gmail.com>
2016-03-03 17:08:04 +0100

    added an assertion

--- test.js
+++ test.js
@@ -1,2 +1,20 @@
 import test from 'ava';
-import moduleHeader from './index';
+import header from './index';
+import fs from 'fs';
+
+const TEMP_FILE = '_temp';
+
+test.before(async () => {
+  await fs.openSync(TEMP_FILE, 'w');
+});
+
+// test.after(async () => {
+//  s = await createServer();
+// });
+
+test('should append a header', async t => {
+  await header(TEMP_FILE, {'name': 'test'});
+  let contents = await fs.readFileSync(TEMP_FILE);
+
+  t.true(contents.toString().indexOf('test') > 0);
+});
