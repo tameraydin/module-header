96a5d22c3f67fc54befb8793583a05e40afeda6b
Tamer Aydin <tamer.aydin@gmail.com>
2016-03-04 00:03:18 +0100

    updated specs

--- test.js
+++ test.js
@@ -8,13 +8,13 @@ test.before(async () => {
   await fs.openSync(TEMP_FILE, 'w');
 });
 
-// test.after(async () => {
-//  s = await createServer();
-// });
+test.after(async () => {
+  await fs.unlinkSync(TEMP_FILE);
+});
 
 test('should append a header', async t => {
   await header(TEMP_FILE, {'name': 'test'});
-  let contents = await fs.readFileSync(TEMP_FILE);
 
-  t.true(contents.toString().indexOf('test') > 0);
+  let contents = await fs.readFileSync(TEMP_FILE);
+  t.true(contents.indexOf('test') > 0);
 });
